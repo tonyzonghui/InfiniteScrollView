8949388fc0ba9e373ff33c1a88fd800e738c36bd
Zhang Zonghui <zhangzonghui01@gmail.com>
2012-10-18 19:57:17 +0800

    Initial Commit

---
+++ TestInfiniteScrollView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestInfiniteScrollView
+//
+//  Created by Zonghui Zhang on 18/10/12.
+//  Copyright (c) 2012 Zhang Zonghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
